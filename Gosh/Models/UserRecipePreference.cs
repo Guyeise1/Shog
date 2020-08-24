1828cdcc77099c3ca59e6638d0fd16ab969b883a
Guy Eisenbach <guyeise5@gmail.com>
2020-08-24 12:45:00 +0300

    A model to enable statistics about user preferences

---
+++ Gosh/Models/UserRecipePreference.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Gosh.Models
+{
+    /// <summary>
+    /// This class is for statistics about userpreferences of recipes.
+    /// Every time a user clicks on a recipe a new row of this model inserted into
+    /// the database.
+    /// </summary>
+    public class UserRecipePreference
+    {
+        public long ID { get; set; }
+
+        [ForeignKey("User")]
+        public long UserID { get; set; }
+
+        public User User { get; set; }
+
+        [ForeignKey("Recipe")]
+        public long RecipeID { get; set; }
+
+        public Recipe Recipe { get; set; }
+    }
+}
