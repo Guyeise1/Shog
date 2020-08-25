7a494a43ec63918a0991410c1f196593dc26dc9f
שירז בהר <shirazbehar@gmail.com>
2020-08-25 19:02:26 +0300

    added pie chart with years - no content

--- Gosh/Controllers/GraphController.cs
+++ Gosh/Controllers/GraphController.cs
@@ -15,6 +15,8 @@ namespace Gosh.Controllers
     {
         public string CategoryName { get; set; }
         public int NumOfRecipes { get; set; }
+        public int year { get; set; }
+        public int recipes { get; set; }
     }
     public class GraphController : Controller
     {
@@ -28,14 +30,18 @@ namespace Gosh.Controllers
 
         public ActionResult GetAllCategories()
         {
-            //var getRecipes = db.Recipes
-            //var query = db.Categories.Select(cat => new { name = cat.CategoryName, count = cat.ID });
-            //return Json(query, JsonRequestBehavior.AllowGet);
 
             var query = db.Database.SqlQuery<QueryResult>(
                 "select CategoryName, (select count(RecipeId) from Recipes r where r.CategoryId = c.ID) as NumOfRecipes from Categories c order by c.CategoryName").ToList();
 
             return Json(query, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetRecipesByYears()
+        {
+            var yearsQuery = db.Database.SqlQuery<QueryResult>(
+                "SELECT YEAR(DateCreated) as year, COUNT(*) as recipes FROM Recipes GROUP BY YEAR(DateCreated)").ToList();
+            return Json(yearsQuery, JsonRequestBehavior.AllowGet);
         }
     }
 }
