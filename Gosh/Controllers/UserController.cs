b3760962cef9555204723838a4d7001341e5061f
Guy Eisenbach <guyeise5@gmail.com>
2020-08-29 14:41:05 +0300

    Fixed edit issue

--- Gosh/Controllers/UserController.cs
+++ Gosh/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -182,12 +183,12 @@ namespace Gosh.Controllers
             {
                 return HttpNotFound();
             }
-            if (IsAdmin() || (Session["Userid"] != null && Session["Username"].ToString().ToUpper() == user.Username.ToUpper()))
+            if (!IsAdmin() && ((Session["Userid"] == null || Session["Username"].ToString().ToUpper() != user.Username.ToUpper())))
             {
-                return View(user);
+                return RedirectToAction("Forbidden");
             }
+            return View(user);
 
-            return RedirectToAction("Forbidden");
         }
 
         // POST: User/Edit/5
@@ -195,15 +196,20 @@ namespace Gosh.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Username,Password,FirstName,LastName,Mail,CreditCard,PhoneNumber")] User user)
+        public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,Mail,PhoneNumber")] User user)
         {
-            if (ModelState.IsValid)
+            if (!IsAdmin() && ((Session["Userid"] == null || Convert.ToInt64(Session["Userid"]) != user.ID)))
             {
-                db.Entry(user).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Forbidden");
             }
-            return View(user);
+            User fromDB = db.Users.Where(u => user.ID == u.ID).First();
+            fromDB.FirstName = user.FirstName;
+            fromDB.LastName = user.LastName;
+            fromDB.Mail = user.Mail;
+            fromDB.PhoneNumber = user.PhoneNumber;
+            db.Users.AddOrUpdate(fromDB);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
 
         // GET: User/Delete/5
